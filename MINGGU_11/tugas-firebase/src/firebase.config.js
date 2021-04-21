f01f541862d625beff1333e174dda5b141cc14dd
fidelatrstyni <fidelatrstyni@gmail.com>
2021-04-21 21:33:59 +0700

    CREATE MINGGU_11

---
+++ MINGGU_11/tugas-firebase/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyA6NPG6N6-m1vWCplS7vjk-MfAQ52TKpVk",
+    authDomain: "modul-firebase-8ba02.firebaseapp.com",
+    projectId: "modul-firebase-8ba02",
+    storageBucket: "modul-firebase-8ba02.appspot.com",
+    messagingSenderId: "478848818092",
+    appId: "1:478848818092:web:624a24e749576c4b927809",
+    measurementId: "G-38H4YLHDMZ"
+};
