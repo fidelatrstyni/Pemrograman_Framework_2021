3c1c8ae3e5facde8abd8b042f624d928549c9acd
fidelatrstyni <fidelatrstyni@gmail.com>
2021-05-05 18:12:15 +0700

    Add Minggu_12

---
+++ MINGGU_12/laravelapp/resources/views/profil.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/css/profil.css">
+<title>BIODATA DIRI</title>
+<body>
+    <h1 align="center">BIODATA DIRI</h1>
+    <hr style="width:750px"/>
+    <table class="table table-hover" style="width:750px;margin-left:300px">
+        <thead>
+            <tr class="table-dark">
+                <th scope="col">No</th>
+                <th scope="col" colspan="1" style="text-align:center">Data Diri</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Nama</td>
+                <td>Fidela Trisaktiyani</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jurusan</td>
+                <td>Teknologi Informasi</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Program Studi</td>
+                <td>Teknik Informatika</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td>Alamat</td>
+                <td>Sidoarjo</td>
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td>Tempat Lahir</td>
+                <td>Surabaya</td>
+            </tr>
+            <tr>
+                <th scope="row">6</th>
+                <td>Tanggal Lahir</td>
+                <td>01 Oktober 1999</td>
+            </tr>
+            <tr>
+                <th scope="row">7</th>
+                <td>Status Mahasiswa</td>
+                <td>Aktif</td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+</html>
